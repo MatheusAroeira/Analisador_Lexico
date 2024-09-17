5878ec7c4855f621ab332b799d4dab55c24f1bc5
MATHEUS FELIPE LIMA AROEIRA <matheus.00000847831@unicap.br>
2024-09-17 11:41:38 -0300

    remoção de espaços em brancos e tabulaçoes

--- app.java
+++ app.java
@@ -7,21 +7,34 @@ import java.util.Arrays;
 public class app {
 
     public static Object[] arrayLinhas() throws FileNotFoundException {
-        try(Scanner scanner = new Scanner(new File("codigo-fonte.txt"));){
-            ArrayList<String> array = new ArrayList<String>();
-
+        try(Scanner scanner = new Scanner(new File("codigo-fonte.txt"))){
+            ArrayList<String> arrayLinha = new ArrayList<>();
+            StringBuffer lexBuffer;
             String linha;
+
+            //faz a leitura linha a linha e coloca num arrayList
             while (scanner.hasNextLine()) {
                 linha = scanner.nextLine();
-                array.add(linha);
-            }
-            String[] result = new String[array.size()];
-            int cont = 0;
-            for (String line: array) {
-                result[cont] = line;
+                arrayLinha.add(linha);
             }
 
-            return array.toArray();
+            //remove quebra de linhas
+            arrayLinha.removeIf(lex -> lex.isEmpty() || lex.isBlank());
+
+            //remove tabulações
+            for (String lex: arrayLinha) {
+                lexBuffer = new StringBuffer();
+
+                for (int i = 0; i <lex.length() ; i++) {
+                    if(lex.charAt(i) != '\t'){
+                        lexBuffer.append(lex.charAt(i));
+                    }
+                }
+
+                arrayLinha.set(arrayLinha.indexOf(lex), lexBuffer.toString());
+            }
+
+            return arrayLinha.toArray();
         } catch (FileNotFoundException e){
             throw new FileNotFoundException("Arquivo não encontrado");
         }
